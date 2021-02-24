69fd04d04de9576524f624a1f15920845cef7459
programalejandro <79589045+programalejandro@users.noreply.github.com>
2021-02-24 09:51:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+})
